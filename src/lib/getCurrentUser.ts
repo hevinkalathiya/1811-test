9f7548f748eb5f85b8a13d0247692296c5ce74b6
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-09 22:48:00 +0530

    Fix : build error

--- src/lib/getCurrentUser.ts
+++ src/lib/getCurrentUser.ts
@@ -3,6 +3,6 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 export const getCurrentUser = async () => {
     const supabase = createClientComponentClient();
     const { data } = await supabase.auth.getSession();
-
-    return data?.session?.user?.identities[0].identity_data ?? [];
+    //@ts-ignore
+    return data?.session?.user?.identities[0]?.identity_data ?? [];
 };
